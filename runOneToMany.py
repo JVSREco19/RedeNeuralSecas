aa2be3e087adfe1fb90d2b0fc975512503779a7c
Anderson Lima <67929816+A-Infor@users.noreply.github.com>
2024-09-24 12:08:37 -0300

    runOneToMany.py: turning into a ternary expression
    Ternary expression improves readability for very simple if-elses like this one. Also, why print messages for every folder created, instead of printing only when there is an error?

--- runOneToMany.py
+++ runOneToMany.py
@@ -4,12 +4,10 @@ from NeuralNetwork.NeuralNetwork import ApplyTraining, FitNeuralNetwork,PrintMet
 import os
 
 citiesList = [os.path.splitext(arquivo)[0] for arquivo in os.listdir('./Data') if os.path.isfile(os.path.join('./Data', arquivo))]
+
 for city in citiesList:
-  caminho = './Images/'+city
-  if not os.path.exists(caminho):
-    # Cria o diretório se ele não existir
-    os.makedirs(caminho)
-    print(f"Pasta '{caminho}' criada com sucesso.")
+  caminho = './Images/' + city
+  os.makedirs(caminho) if not os.path.exists(caminho) else print(f"Pasta '{caminho}' já existe!")
 
       
 showImages = False
