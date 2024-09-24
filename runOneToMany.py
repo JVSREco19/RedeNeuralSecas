04bae3e29b562555963cec15b3202d596de4473e
Anderson Lima <67929816+A-Infor@users.noreply.github.com>
2024-09-24 09:07:05 -0300

    runOneToMany.py: delete function
    There is no need for a custom function to remove suffixes, as there is already a built-in function for that: https://docs.python.org/3/library/stdtypes.html#str.removesuffix .

--- runOneToMany.py
+++ runOneToMany.py
@@ -3,21 +3,12 @@ from NeuralNetwork.NeuralNetwork import ApplyTraining, FitNeuralNetwork,PrintMet
 
 import os
 
-def remover_extensao(nome_arquivo):
-    # Verifica se o arquivo tem a extensão .xlsx
-    if nome_arquivo.endswith('.xlsx'):
-        # Remove a extensão .xlsx
-        return nome_arquivo[:-5]
-    else:
-        # Retorna o nome do arquivo sem alterações
-        return nome_arquivo
-
 def nomes_arquivos_sem_extensao(caminho_pasta):
     # Lista todos os arquivos no diretório
     arquivos = os.listdir(caminho_pasta)
     
     # Remove a extensão .xlsx de cada nome de arquivo
-    nomes_sem_extensao = [remover_extensao(arquivo) for arquivo in arquivos]
+    nomes_sem_extensao = [arquivo.removesuffix('.xlsx') for arquivo in arquivos]
     
     return nomes_sem_extensao
 
