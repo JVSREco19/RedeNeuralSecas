720e2cee250d763f6adaffce6f44eb1ed06a3acf
Anderson Lima <67929816+A-Infor@users.noreply.github.com>
2024-09-24 12:17:52 -0300

    runOneToMany.py: defining CONSTANTS
    These are not real variables.
    See: https://realpython.com/python-constants/#defining-your-own-constants-in-python .

--- runOneToMany.py
+++ runOneToMany.py
@@ -3,22 +3,21 @@ from NeuralNetwork.NeuralNetwork import ApplyTraining, FitNeuralNetwork,PrintMet
 
 import os
 
+CITY        = 'SÃO JOÃO DA PONTE'
+SHOW_IMAGES = False
+
 citiesList = [os.path.splitext(arquivo)[0] for arquivo in os.listdir('./Data') if os.path.isfile(os.path.join('./Data', arquivo))]
 
 for city in citiesList:
   caminho = './Images/' + city
   os.makedirs(caminho) if not os.path.exists(caminho) else print(f"Pasta '{caminho}' já existe!")
 
-      
-showImages = False
-
-city = 'SÃO JOÃO DA PONTE'
-model = FitNeuralNetwork('./Data/'+city+'.xlsx', city,showImages)
+model = FitNeuralNetwork('./Data/'+CITY+'.xlsx', CITY , SHOW_IMAGES)
 citiesListAux = citiesList.copy()
-citiesListAux.remove(city)
+citiesListAux.remove(CITY)
 
 for cityAux in citiesListAux: 
   #Deve-se passar o caminho para o xlsx, o nome da Região e o modelo treinado;
-  ApplyTraining("./Data/"+cityAux+".xlsx", cityAux, model,showImages,city)
+  ApplyTraining("./Data/"+cityAux+".xlsx", cityAux, model, SHOW_IMAGES, CITY)
   
 PrintMetricsList()
