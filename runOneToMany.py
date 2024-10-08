57f1343772f177c63a291833bb52db5d8b9d80c3
Anderson Lima <67929816+A-Infor@users.noreply.github.com>
2024-10-08 11:25:32 -0300

    `runOneToMany.py`: making it work with 2 levels of folder structure
    Two levels of folder structure are needed in order to work with more than one city cluster at the same time.

--- runOneToMany.py
+++ runOneToMany.py
@@ -1,23 +1,66 @@
 #Deve-se passar o caminho para o xlsx da região para qual o modelo deve ser TREINADO
 from NeuralNetwork.NeuralNetwork import ApplyTraining, FitNeuralNetwork,PrintMetricsList
 
+import pandas as pd
+import numpy as np
+import json
 import os
 
-CITY        = 'SÃO JOÃO DA PONTE'
+def define_cities_of_interest(rootdir):
+    # Gets the names of the directories inside rootdir and defines them as the central cities:
+    central_city_names   = os.listdir(rootdir)
+    
+    # Gets the names of the subdirectories inside each central city directory and defines them as the corresponding bordering cities:
+    dict_cities_of_interest = {}
+    for central_city_name in central_city_names:
+        bordering_cities_filenames = os.listdir(f'{rootdir}/{central_city_name}/')
+        
+        bordering_cities_names= []
+        for bordering_city_filename in bordering_cities_filenames:
+            bordering_city_name = bordering_city_filename.rstrip('.xlsx')
+            bordering_cities_names.append(bordering_city_name)
+            
+        # To avoid duplication of the central_city_name inside the bordering_cities_names:
+        bordering_cities_names.remove(central_city_name)
+            
+        dict_cities_of_interest[central_city_name] = bordering_cities_names
+        
+    return dict_cities_of_interest
+
+def create_empty_image_directory_tree(dict_cities_of_interest, rootdir):
+    for central_city, list_of_bordering_cities in dict_cities_of_interest.items():
+        os.makedirs(f'{rootdir}/{central_city}/{central_city}')
+        
+        for bordering_city in list_of_bordering_cities:
+            os.makedirs(f'{rootdir}/{central_city}/{bordering_city}')
+    
+def create_neural_network_models_for_central_cities(dict_cities_of_interest, rootdir):
+    neural_network_models = {}
+    
+    for central_city in dict_cities_of_interest.keys():
+        model, metricsCompendium = FitNeuralNetwork(f'{rootdir}/{central_city}/{central_city}.xlsx', central_city, central_city, SHOW_IMAGES)
+        neural_network_models[central_city] = model
+    return neural_network_models, metricsCompendium
+
+def apply_neural_network_models_for_bordering_cities(dict_cities_of_interest, neural_network_models, rootdir):
+    for central_city, list_of_bordering_cities in dict_cities_of_interest.items():
+        for bordering_city in list_of_bordering_cities:
+            metricsCompendium = ApplyTraining(f'{rootdir}/{central_city}/{bordering_city}.xlsx', central_city, bordering_city, neural_network_models[central_city], SHOW_IMAGES, bordering_city)
+    return metricsCompendium
+
+
 SHOW_IMAGES = False
 
-citiesList = [os.path.splitext(arquivo)[0] for arquivo in os.listdir('./Data') if os.path.isfile(os.path.join('./Data', arquivo))]
+dict_cities_of_interest = define_cities_of_interest('./Data')
 
-for city in citiesList:
-  caminho = './Images/' + city
-  os.makedirs(caminho) if not os.path.exists(caminho) else print(f"Pasta '{caminho}' já existe!")
+create_empty_image_directory_tree(dict_cities_of_interest, './Images')
 
-model = FitNeuralNetwork('./Data/'+CITY+'.xlsx', CITY , SHOW_IMAGES)
-citiesListAux = citiesList.copy()
-citiesListAux.remove(CITY)
+print('TRAINING: START')
+neural_network_models, metricsCompendium = create_neural_network_models_for_central_cities(dict_cities_of_interest, './Data')
+print('TRAINING: END')
 
-for cityAux in citiesListAux: 
-  #Deve-se passar o caminho para o xlsx, o nome da Região e o modelo treinado;
-  ApplyTraining("./Data/"+cityAux+".xlsx", cityAux, model, SHOW_IMAGES, CITY)
-  
-PrintMetricsList()
+print('APPLYING: START')
+metricsCompendium = apply_neural_network_models_for_bordering_cities(dict_cities_of_interest, neural_network_models, './Data')
+print('APPLYING: END')
+
+PrintMetricsList(metricsCompendium)
