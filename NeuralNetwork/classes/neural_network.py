334f9f2481a4a1e3f586e898e9e8d9cdac2dc8dc
A-Infor <anderson.informatica.mg@gmx.com>
2026-04-11 10:31:59 -0300

    Fixed wrong input shape for Sliding Windows, and did other adjustments for running both techniques less baldy.

--- NeuralNetwork/classes/neural_network.py
+++ NeuralNetwork/classes/neural_network.py
@@ -12,9 +12,12 @@ class NeuralNetwork:
         self.evaluator      = PerformanceEvaluator()
         
         self.configs_dict   = self._set_configs(file_name)
-        self.model_sliding  = self._create_ml_model()
-        self.model_tumbling = self._create_ml_model()
+        self.model_sliding  = self._create_ml_model('sliding' )
+        self.model_tumbling = self._create_ml_model('tumbling')
         self.has_trained    = False
+        
+        print(f"input_shape_sliding : {self.configs_dict['input_shape_sliding' ]};")
+        print(f"input_shape_tumbling: {self.configs_dict['input_shape_tumbling']}.")
         
         # print('Input shape:', self.model.input_shape)
         # print(self.model.summary())
@@ -22,9 +25,10 @@ class NeuralNetwork:
     def _set_configs(self, file_name):
         with open(file_name) as file:
             configs_dict = json.load(file)
-        
+
         configs_dict.update(
-            {'input_shape' : (configs_dict['total_points'] - configs_dict['dense_units'], 1),
+            {'input_shape_sliding' : (configs_dict['sliding_lookback_len' ], 1),
+             'input_shape_tumbling': (configs_dict['tumbling_lookback_len'], 1),
              'activation'  : ['relu', 'sigmoid'],
              'loss'        : 'mse',
              'metrics'     : ['mae',
@@ -37,15 +41,15 @@ class NeuralNetwork:
         
         return configs_dict        
 
-    def _create_ml_model(self):
+    def _create_ml_model(self, technique):
         # print(f'Started: creation of ML model {self.dataset.city_name}')
         model = tf.keras.Sequential()
-        model.add(tf.keras.Input       (    shape=self.configs_dict['input_shape' ])      )
-        model.add(tf.keras.layers.LSTM (          self.configs_dict['hidden_units']       ,
-                                       activation=self.configs_dict['activation'  ][0])   )
+        model.add(tf.keras.Input       (    shape=self.configs_dict[f'input_shape_{technique}' ]) )
+        model.add(tf.keras.layers.LSTM (          self.configs_dict[ 'hidden_units']       ,
+                                       activation=self.configs_dict[ 'activation'  ][0])   )
         for _ in range(3):
-            model.add(tf.keras.layers.Dense(units=self.configs_dict['dense_units' ]       ,
-                                       activation=self.configs_dict['activation'][1])     )
+            model.add(tf.keras.layers.Dense(units=self.configs_dict[ 'dense_units' ]       ,
+                                       activation=self.configs_dict[ 'activation'][1])     )
         model.compile(loss=self.configs_dict['loss'], metrics=self.configs_dict['metrics'],
                       optimizer=self.configs_dict['optimizer']                            )
         # print(f'Ended: creation of ML model {self.dataset.city_name}')
@@ -61,7 +65,8 @@ class NeuralNetwork:
         history_sliding = self.model_sliding.fit(
             spei_provided_inputs_sliding  ['80%'],
             spei_expected_outputs_sliding ['80%'],
-            epochs=self.configs_dict['numberOfEpochs'], batch_size=1, verbose=0)
+            # Attempted, failed, batch_sizes: 256, 64, 32, 16, 1.
+            epochs=self.configs_dict['numberOfEpochs'], batch_size= 64, verbose=0)
         self.has_trained = True
         print(f'Ended  : training of ML model {self.dataset.city_name}, sliding windows' )
         
@@ -69,7 +74,7 @@ class NeuralNetwork:
         history_tumbling = self.model_tumbling.fit(
             spei_provided_inputs_tumbling  ['80%'],
             spei_expected_outputs_tumbling ['80%'],
-            epochs=self.configs_dict['numberOfEpochs'], batch_size=1, verbose=0)
+            epochs=self.configs_dict['numberOfEpochs'], batch_size=  1, verbose=0)
         self.has_trained = True
         print(f'Ended  : training of ML model {self.dataset.city_name}, tumbling windows')
         
